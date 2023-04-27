83c2098d7471faf6610ad63ee79c1ce323db048d
Anastasiia Buhaienko <anastasiia.buhaienko@gmail.com>
2023-04-26 21:30:07 -0500

    add clsx

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,19 +1,32 @@
 import PropTypes from 'prop-types';
 import css from './FriendListItem.module.css';
+import clsx from 'clsx';
 
 export const FriendListItem = ({avatar, name, isOnline}) => {
     return (
         <li className={css.item}>
 
-            <span 
+             {/* вариант добавление дополнительного класса в зависимости от условия */}
+            {/* <span 
                 className={
                     isOnline
                     ? `${css.status} ${css.online}`
                     : `${css.status} ${css.offline}`
                 }
             >
+            </span> */}
+
+            {/* c помощью пакета clsx можно написать добавление класса по другому */}
+            <span 
+                className={clsx(
+                    css.status, 
+                    isOnline ? css.online : css.offline
+                )}
+            >
             </span>
-            
+
+
+
             <img className= {css.avatar} src={avatar} alt="User avatar" width="48" />
             <p className={css.name}>{name}</p>
             
